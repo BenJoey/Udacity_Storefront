0e60b0f549dcb90abfe00b3137161cd7342badd9
Bence <bauerbence5@gmail.com>
2021-09-03 23:26:32 +0200

    added jwts

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -1,5 +1,9 @@
 import express, { Request, Response } from 'express';
 import { User, UserStore } from '../models/users';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const store = new UserStore();
 
@@ -23,7 +27,8 @@ const create = async (req: Request, res: Response) => {
     };
 
     const newUser = await store.create(user);
-    res.json(newUser);
+    var token = jwt.sign({ user: newUser }, process.env.TOKEN_SECRET as string);
+    res.json(token);
   } catch (err) {
     res.status(400);
     res.json(err);
@@ -33,6 +38,23 @@ const create = async (req: Request, res: Response) => {
 const destroy = async (req: Request, res: Response) => {
   const deleted = await store.delete(req.body.id);
   res.json(deleted);
+};
+
+const authenticate = async (req: Request, res: Response) => {
+  const user: User = {
+    username: req.body.username,
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    password: req.body.password
+  };
+  try {
+    const u = await store.authenticate(user.username, user.password);
+    var token = jwt.sign({ user: u }, process.env.TOKEN_SECRET as string);
+    res.json(token);
+  } catch (error) {
+    res.status(401);
+    res.json({ error });
+  }
 };
 
 const userRoutes = (app: express.Application) => {
