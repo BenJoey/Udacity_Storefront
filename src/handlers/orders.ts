71c58957eaa08196d4b8a8b877f77bf031be1f15
Bence <bauerbence5@gmail.com>
2021-09-03 14:11:58 +0200

    created handler and model for orders

---
+++ src/handlers/orders.ts
@@ -0,0 +1,59 @@
+import express, { Request, Response } from 'express';
+import { Order, OrderStore } from '../models/orders';
+
+const store = new OrderStore();
+
+const index = async (_req: Request, res: Response) => {
+  const orders = await store.index();
+  res.json(orders);
+};
+
+const show = async (req: Request, res: Response) => {
+  const orders = await store.show(req.body.id);
+  res.json(orders);
+};
+
+const create = async (req: Request, res: Response) => {
+  try {
+    const order: Order = {
+      user_id: req.body.user_id,
+      status: 'open'
+    };
+
+    const newOrder = await store.create(order);
+    res.json(newOrder);
+  } catch (err) {
+    res.status(400);
+    res.json(err);
+  }
+};
+
+const destroy = async (req: Request, res: Response) => {
+  const deleted = await store.delete(req.body.id);
+  res.json(deleted);
+};
+
+const addProduct = async (_req: Request, res: Response) => {
+  const orderId: string = _req.params.id;
+  const productId: string = _req.body.productId;
+  const quantity: number = parseInt(_req.body.quantity);
+
+  try {
+    const addedProduct = await store.addProduct(quantity, orderId, productId);
+    res.json(addedProduct);
+  } catch (err) {
+    res.status(400);
+    res.json(err);
+  }
+};
+
+const orderRoutes = (app: express.Application) => {
+  app.get('/orders', index);
+  app.get('/orders/:id', show);
+  app.post('/orders', create);
+  app.delete('/orders', destroy);
+  // add product
+  app.post('/orders/:id/products', addProduct);
+};
+
+export default orderRoutes;
---
+++ src/models/orders.ts
@@ -0,0 +1,103 @@
+import client from '../database';
+
+export type Order = {
+  id?: string;
+  user_id: number;
+  status: string;
+};
+
+export class OrderStore {
+  async index(): Promise<Order[]> {
+    try {
+      // @ts-ignore
+      const conn = await client.connect();
+      const sql = 'SELECT * FROM orders';
+
+      const result = await conn.query(sql);
+
+      conn.release();
+
+      return result.rows;
+    } catch (err) {
+      throw new Error(`Could not get orders. Error: ${err}`);
+    }
+  }
+
+  async show(id: string): Promise<Order> {
+    try {
+      const sql = 'SELECT * FROM orders WHERE id=($1)';
+      // @ts-ignore
+      const conn = await client.connect();
+
+      const result = await conn.query(sql, [id]);
+
+      conn.release();
+
+      return result.rows[0];
+    } catch (err) {
+      throw new Error(`Could not get order ${id}. Error: ${err}`);
+    }
+  }
+
+  async create(order: Order): Promise<Order> {
+    try {
+      const sql =
+        'INSERT INTO orders (user_id, status) VALUES($1, $2) RETURNING *';
+      // @ts-ignore
+      const conn = await client.connect();
+
+      const result = await conn.query(sql, [order.user_id, order.status]);
+
+      const ord = result.rows[0];
+
+      conn.release();
+
+      return ord;
+    } catch (err) {
+      throw new Error(`Could not add order. Error: ${err}`);
+    }
+  }
+
+  async delete(id: string): Promise<Order> {
+    try {
+      const sql = 'DELETE FROM orders WHERE id=($1)';
+      // @ts-ignore
+      const conn = await client.connect();
+
+      const result = await conn.query(sql, [id]);
+
+      const order = result.rows[0];
+
+      conn.release();
+
+      return order;
+    } catch (err) {
+      throw new Error(`Could not delete order. Error: ${err}`);
+    }
+  }
+
+  async addProduct(
+    quantity: number,
+    orderId: string,
+    productId: string
+  ): Promise<Order> {
+    try {
+      const sql =
+        'INSERT INTO order_products (quantity, order_id, product_id) VALUES($1, $2, $3) RETURNING *';
+      //@ts-ignore
+      const conn = await Client.connect();
+
+      const result = await conn.query(sql, [quantity, orderId, productId]);
+
+      const order = result.rows[0];
+
+      conn.release();
+
+      return order;
+    } catch (err) {
+      throw new Error(
+        `Could not add product ${productId} to order ${orderId}: ${err}`
+      );
+    }
+  }
+}
