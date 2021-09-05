e2dd041e9c055d0b191967a61a04326259562d98
Bence <bauerbence5@gmail.com>
2021-09-05 18:51:55 +0200

    started db tests

--- src/database.ts
+++ src/database.ts
@@ -3,12 +3,20 @@ import { Pool } from 'pg';
 
 dotenv.config();
 
-const { POSTGRES_HOST, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD } =
-  process.env;
+const {
+  POSTGRES_HOST,
+  POSTGRES_DB,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  POSTGRES_TEST_DB,
+  ENV
+} = process.env;
+
+let db = ENV === 'dev' ? POSTGRES_DB : POSTGRES_TEST_DB;
 
 const client = new Pool({
   host: POSTGRES_HOST,
-  database: POSTGRES_DB,
+  database: db,
   user: POSTGRES_USER,
   password: POSTGRES_PASSWORD
 });
